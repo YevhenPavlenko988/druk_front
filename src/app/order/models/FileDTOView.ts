eece41c7f288ab8933871ba8db68be0a3cae0858
@tytarenkons <tytarenkoNS@softbees.dev>
2025-04-29 13:11:22 +0300

    printer-list

--- src/app/order/models/FileDTOView.ts
+++ src/app/order/models/FileDTOView.ts
@@ -1,7 +1,7 @@
 import {FileDTO} from './FileDTO';
 import {PrintTypeEnum} from './PrintTypeEnum';
 
-export interface FileView extends FileDTO {
+export interface FileDTOView extends FileDTO {
     $name?: string,
     $printType?: PrintTypeEnum,
     $copiesCount?: number
--- src/app/order/services/printers.service.ts
+++ src/app/order/services/printers.service.ts
@@ -1,39 +1,43 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Observable, of} from 'rxjs';
+import {map, Observable, of, tap} from 'rxjs';
 import {environment} from '../../../environments/environment';
 import {PrinterDTO} from '../models/PrinterDTO';
+import {joinUrl} from '../../$core/utils/joinUrl';
+import {PrinterListDTO} from '../models/PrinterListDTO';
 
 
-const printerList: Array<PrinterDTO> = [
-    {
-        id: '0f75c925-0652-42e9-8b8f-829435ab0c27',
-        name: 'принтер 1',
-        paperLeft: 167,
-        paperMax: 600,
-        address: 'Київ, вулиця 1, буд 4',
-        latitude: 222,
-        longitude: 444,
-    },
-    {
-        id: '9a765319-ff44-491f-ba58-c67c906cf68c',
-        name: 'Принтер 6',
-        paperLeft: 67,
-        paperMax: 500,
-        address: 'Дніпро, вулиця 3, буд 454',
-        latitude: 666,
-        longitude: 777,
-    },
-    {
-        id: '3a765319-ff44-491f-ba58-c67c906cf690',
-        name: 'Принтер 643',
-        paperLeft: 67,
-        paperMax: 500,
-        address: 'Дніпро, вулиця 13, буд 54',
-        latitude: 666,
-        longitude: 777,
-    },
-];
+// API
+export const API_URL_PRINTER_LIST: string = '/printer/list';
+
+
+const printerList: PrinterListDTO = {
+    printersList: [
+        {
+            id: 'fd5b113f-84fe-4fc8-b43d-bd7596c49eb2',
+            createdAt: '2025-04-18T15:59:43.410132',
+            updatedAt: '2025-04-26T19:43:36.178162',
+            name: 'Test printer',
+            ip: '192.168.1.103',
+            url: 'www.test.printer.com',
+            paperLeft: 0,
+            paperMax: 2600,
+            address: 'Кіїв, вул. Вулиця, буд 1',
+            latitude: 700,
+            longitude: 800,
+            isActive: true,
+            isOnline: true,
+            photoUrl: 'www.photo.url',
+            model: 'testModel',
+            serialNumber: 'testSN',
+            note: 'note',
+            schedule: '24/7',
+            priceOneSide: 5,
+            priceDuplex: 7,
+            serviceFee: 3
+        },
+    ]
+};
 
 @Injectable({providedIn: 'root'})
 export class PrintersService {
@@ -45,6 +49,26 @@ export class PrintersService {
     }
 
     getPrinterList(): Observable<Array<PrinterDTO>> {
-        return of(printerList);
+        // return of(printerList.printersList);
+
+        const url: string = joinUrl(environment.apiUrl, API_URL_PRINTER_LIST);
+        if (environment.log.debug) {
+            console.log(`GET ${url}`);
+        }
+        return this.http.get(url).pipe(
+            tap({
+                next: (resp: any) => {
+                    if (environment.log.debug) {
+                        console.log(`RESULT GET: ${url}`, resp);
+                    }
+                },
+                error: (err: any) => {
+                    if (environment.log.error) {
+                        console.error(`RESULT GET: ${url} Error: `, err);
+                    }
+                },
+            }),
+            map((r: PrinterListDTO) => r?.printersList),
+        );
     }
 }
